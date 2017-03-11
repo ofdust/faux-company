c069d32f9b73c294db1cd6b0d78c59f805466ba7
Shelby <captain.norcross@gmail.com>
2017-03-11 11:32:07 -0700

    created nav fade in functionality when you scroll

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 //var i = 0;
 //var images = ["img/bridge.jpg", "img/buckingham.jpg", "img/tower.jpg"];
 
+var headerColor = "rgba(240, 240, 240,";
+
 function changeImage() {
 	var currentImage = ("#gallery figure.active");
 	var nextImage;
@@ -29,7 +31,61 @@ checkScroll();
 $(window).scroll(checkScroll);
 function checkScroll() {
 	var top = $(window).scrollTop();
+	if(top > 0) {
+		$("nav").css("backgroundColor", headerColor + "0)", 500);
+	}
+	if(top > 80) {
+		$("nav").css("backgroundColor", headerColor + ".05", 500);
+	}
 	if(top > 100) {
-		$("nav").css("backgroundColor", "#f0f0f0", 500);
+		$("nav").css("backgroundColor", headerColor + ".1", 500);
+	}
+	if(top > 110) {
+		$("nav").css("backgroundColor", headerColor + ".15", 500);
+	}
+	if(top > 120) {
+		$("nav").css("backgroundColor", headerColor + ".3", 500);
+	}
+	if(top > 130) {
+		$("nav").css("backgroundColor", headerColor + ".35", 500);
+	}
+	if(top > 140) {
+		$("nav").css("backgroundColor", headerColor + ".4", 500);
+	}
+	if(top > 150) {
+		$("nav").css("backgroundColor", headerColor + ".45", 500);
+	}
+	if(top > 160) {
+		$("nav").css("backgroundColor", headerColor + ".5", 500);
+	}
+	if(top > 170) {
+		$("nav").css("backgroundColor", headerColor + ".55", 500);
+	}
+	if(top > 180) {
+		$("nav").css("backgroundColor", headerColor + ".6", 500);
+	}
+	if(top > 190) {
+		$("nav").css("backgroundColor", headerColor + ".65", 500);
+	}
+	if(top > 200) {
+		$("nav").css("backgroundColor", headerColor + ".7", 500);
+	}
+	if(top > 210) {
+		$("nav").css("backgroundColor", headerColor + ".75", 500);
+	}
+	if(top > 220) {
+		$("nav").css("backgroundColor", headerColor + ".8", 500);
+	}
+	if(top > 230) {
+		$("nav").css("backgroundColor", headerColor + ".85", 500);
+	}
+	if(top > 240) {
+		$("nav").css("backgroundColor", headerColor + ".9", 500);
+	}
+	if(top > 250) {
+		$("nav").css("backgroundColor", headerColor + ".95", 500);
+	}
+	if(top > 260) {
+		$("nav").css("backgroundColor", headerColor + "1", 500);
 	}
 }
