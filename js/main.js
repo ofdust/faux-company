a9d5cdf5e62a9b6277d04fcc16d3741f2a1c2a7e
Shelby <captain.norcross@gmail.com>
2017-03-12 13:09:18 -0600

    added scrolling of navbar and nav link hover states

--- js/main.js
+++ js/main.js
@@ -154,3 +154,19 @@ function initMap() {
         	map: map
         });
       }
+
+
+// shout out to https://css-tricks.com/snippets/jquery/smooth-scrolling/ for this scrolling help
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top - 90}, 1000);
+        return false;
+      }
+    }
+  });
+});
